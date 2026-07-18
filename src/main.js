d2d3185708f3c3f7e790393c34246021d0d2e6c9
Pils10 <support@pils10.de>
2026-07-18 23:53:59 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <section class="card">
     <p class="eyebrow">KIND · JENKINS · ARGO CD</p>
-    <h1>Hello from the GitOps lab!</h1>
+    <h1>Big change and issues fixed!!</h1>
     <p>
       This page was built by Jenkins, stored as a container image,
       declared in Git, and reconciled by Argo CD.
     </p>
     <code>Change this text, push it, and watch dev update.</code>
-	<p>Small Dev Change</p>
+	<p>Last Check before publish</p>
   </section>
 `
